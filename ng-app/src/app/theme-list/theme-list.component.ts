bf8d73a6afb87077125c6d469f19da2e778de04d
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-28 20:19:52 +0300

    render themes from rest api

--- ng-app/src/app/theme-list/theme-list.component.ts
+++ ng-app/src/app/theme-list/theme-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { ITheme } from '../interfaces/theme';
 
 @Component({
   selector: 'app-theme-list',
@@ -7,12 +8,15 @@ import { ApiService } from '../api.service';
   styleUrls: ['./theme-list.component.scss'],
 })
 export class ThemeListComponent implements OnInit {
+
+  themeList: ITheme[] | null = null;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getThemes().subscribe({
       next: (val) => {
-        console.log(val);
+        this.themeList = val;
       },
       error: (err) => {
         console.error(err);
