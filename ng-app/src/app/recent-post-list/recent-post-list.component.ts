45ce99d132170160aaae419044977c5126db8adc
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-28 20:28:22 +0300

    render posts from rest api

--- ng-app/src/app/recent-post-list/recent-post-list.component.ts
+++ ng-app/src/app/recent-post-list/recent-post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { IPost } from '../interfaces/post';
 
 @Component({
   selector: 'app-recent-post-list',
@@ -7,12 +8,14 @@ import { ApiService } from '../api.service';
   styleUrls: ['./recent-post-list.component.scss'],
 })
 export class RecentPostListComponent {
+  postList: IPost[] | null = null;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getPosts(5).subscribe({
       next: (val) => {
-        console.log(val);
+        this.postList = val;
       },
       error: (err) => {
         console.error(err);
