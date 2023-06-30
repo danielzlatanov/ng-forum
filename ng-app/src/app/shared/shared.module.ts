3314b9ac452da213c0e6c738c9df6026484be7d4
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-30 23:23:05 +0300

    reuse welcome cmp both in home and main cmps

--- ng-app/src/app/shared/shared.module.ts
+++ ng-app/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [LoaderComponent],
-  imports: [CommonModule],
-  exports: [LoaderComponent],
+  declarations: [LoaderComponent, WelcomeComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [LoaderComponent, WelcomeComponent],
 })
 export class SharedModule {}
