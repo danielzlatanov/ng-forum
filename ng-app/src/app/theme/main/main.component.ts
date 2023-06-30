ac6f5c990888ec890de73f4d62bc1809d459ac80
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-30 23:26:54 +0300

    hide welcome from main depending on logged-in status

--- ng-app/src/app/theme/main/main.component.ts
+++ ng-app/src/app/theme/main/main.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements DoCheck {
   isLoggedIn = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngDoCheck(): void {
+    this.isLoggedIn = this.authService.isLoggedIn;
+  }
 }
