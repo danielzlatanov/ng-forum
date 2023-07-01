102cca7f3b5b71d1613d8c3bd73a5ecfddb661be
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-01 14:55:54 +0300

    add welcome cmp to comments for guests; fix styles

--- ng-app/src/app/theme/theme-content/theme-content.component.ts
+++ ng-app/src/app/theme/theme-content/theme-content.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-theme-content',
   templateUrl: './theme-content.component.html',
-  styleUrls: ['./theme-content.component.scss']
+  styleUrls: ['./theme-content.component.scss'],
 })
 export class ThemeContentComponent {
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
+  }
 
+  constructor(private authService: AuthService) {}
 }
