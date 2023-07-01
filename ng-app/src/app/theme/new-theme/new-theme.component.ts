cacbe675a37fae891fea4f3c4497adfbdfb8259f
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-01 15:39:51 +0300

    new theme cancel redirect; temporary save form data

--- ng-app/src/app/theme/new-theme/new-theme.component.ts
+++ ng-app/src/app/theme/new-theme/new-theme.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-new-theme',
   templateUrl: './new-theme.component.html',
-  styleUrls: ['./new-theme.component.scss']
+  styleUrls: ['./new-theme.component.scss'],
 })
 export class NewThemeComponent {
+  constructor(private router: Router, private apiService: ApiService) {}
 
+  createTheme(e: SubmitEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    console.log(formData);
+  }
+
+  cancelHandler() {
+    this.router.navigate(['/']);
+  }
 }
